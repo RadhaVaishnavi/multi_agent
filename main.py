00c0cf8fe1209350bdccdc999a244169712ac05d
RadhaVaishnavi <84319477+RadhaVaishnavi@users.noreply.github.com>
2024-11-26 16:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     company_name = input("Please enter the company's name: ")
     
     inputs = f"Company Name: {company_name}"
-    market_research_crew = MarketResearchCrew(inputs)
-    result = market_research_crew.run()
+    market_research_crew = MarketResearchAgents(inputs)
+    result = market_research_agent.run()
 
     print("\n\n##############################")
     print("## Here are the results of your market research:")
