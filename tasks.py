c11ad9e644d74dcee3e7d6d70c3ab75a730cd7b1
RadhaVaishnavi <84319477+RadhaVaishnavi@users.noreply.github.com>
2024-11-26 16:55:43 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,26 +1,25 @@
 from crewai import Task
 
 class MarketResearchTasks:
-
     def research_task(self, agent, inputs):
-      return Task(
-          agent=agent,
-          description=f"Identify the industry of {inputs} and gather relevant product information.",
-          expected_output=f"Industry classification and product information."
-      )
+        return Task(
+            agent=agent,
+            description=f"Identify the industry for {inputs['company_name']} and gather relevant product information.",
+            expected_output="Industry classification and product details.",
+        )
 
     def use_case_task(self, agent, context):
-      return Task(
-          agent=agent,
-          context=context,
-          description="Analyze industry trends and propose relevant AI/ML use cases.",
-          expected_output=f"List of proposed AI/ML use cases based on industry trends."
-      )
+        return Task(
+            agent=agent,
+            context=context,
+            description="Analyze industry trends and propose AI/ML use cases.",
+            expected_output="List of proposed AI/ML use cases.",
+        )
 
     def resource_task(self, agent, context, inputs):
-      return Task(
-          agent=agent,
-          context=context,
-          description=f"Find datasets related to proposed use cases from platforms like Kaggle or GitHub.",
-          expected_output=f"Links to relevant datasets and github."
-      )
+        return Task(
+            agent=agent,
+            context=context,
+            description=f"Find datasets related to {inputs['company_name']} from Kaggle or GitHub.",
+            expected_output="Links to relevant datasets and GitHub resources.",
+        )
