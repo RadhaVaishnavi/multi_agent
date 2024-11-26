0f153305399eeb8c1e308a24abd34ec6abd0878c
RadhaVaishnavi <84319477+RadhaVaishnavi@users.noreply.github.com>
2024-11-26 16:35:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,14 +31,11 @@ class MarketResearchAgents:
         return crew.kickoff()
 
 if __name__ == "__main__":
-    print("Welcome to the Market Research Crew Setup")
+    print("Welcome to the Market Research ")
     company_name = input("Please enter the company's name: ")
     
     inputs = f"Company Name: {company_name}"
     market_research_agent = MarketResearchAgents(inputs)
     result = market_research_agent.run()
-
-    print("\n\n##############################")
     print("## Here are the results of your market research:")
-    print("##############################\n")
     print(result)
